92c0db3e05ab1f0a9089ec39ce25b0a91361df85
VishweshMS <55580232+VishweshMS@users.noreply.github.com>
2023-01-24 16:34:52 -0500

    update-2

--- helloworld.py
+++ helloworld.py
@@ -7,10 +7,10 @@ import datetime
 #fuction to define scrren brand
 def my_brand(assignment_name):
     print("=*=*=*= Vishwesh Malur Somashekar =*=*=*=")
-    print("=*=*=*= Course 2023S-SSW567-WS =*=*=*=")
+    print("=*=*=*= Course 2023S-SSW567 =*=*=*=")
     print("=*=*=*= " + assignment_name + " =*=*=*=")
     print("=*=*=*= " + str(datetime.datetime.now()) + " =*=*=*=")
 
-my_brand("HW 00")
-print("Hello World")
-my_brand("HW 00")
+my_brand("HW 00 - Tools Setup")
+print("\nHello World\n")
+my_brand("HW 00 - Tools Setup")
