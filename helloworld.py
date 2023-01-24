2d91032ef53c1d1b5d3827cbc651dc8221ceb1ab
VishweshMS <55580232+VishweshMS@users.noreply.github.com>
2023-01-24 16:18:06 -0500

    initial commit

---
+++ helloworld.py
@@ -0,0 +1,12 @@
+import datetime
+
+#fuction to define scrren brand
+def my_brand(assignment_name):
+    print("=*=*=*= Vishwesh Malur Somashekar =*=*=*=")
+    print("=*=*=*= Course 2023S-SSW567-WS =*=*=*=")
+    print("=*=*=*= " + assignment_name + " =*=*=*=")
+    print("=*=*=*= " + str(datetime.datetime.now()) + " =*=*=*=")
+
+my_brand("HW 00")
+print("Hello World")
+my_brand("HW 00")
